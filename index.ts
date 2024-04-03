23b25a9f924dd7b9a767557c96dc45bf074ce737
zaynscode <zaynrajput52@gmail.com>
2024-04-03 09:42:06 +0500

    Adding a number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\tWelcome to Zain's Number Guessing Game\n");
+
+const randomNumber = Math.floor(Math.random()*5+1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"userGuessNumber",
+        type: "number",
+        message: "Enter your guess Number(Number Limit from 1 to 5):",
+    },
+]);
+
+if (answer.userGuessNumber === randomNumber){
+    console.log("Congratulations!You guess a correct Number.");
+}
+else{
+    console.log("Sorry,you guess a wrong number");
+}
